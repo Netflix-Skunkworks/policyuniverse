ea5b699b748fb6bb2f7dd688cc7e8ce367f71420
Nag Medida <nmedida@netflix.com>
2019-05-15 11:02:53 -0700

    Bumping the vesion to 1.3.1.2 changes to data.json

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='policyuniverse',
-      version='1.3.1.1',
+      version='1.3.1.2',
       description='AWS IAM Policy Utilities',
       author='Patrick Kelley',
       author_email='pkelley@netflix.com',
