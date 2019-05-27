64d4ae98824558c4beceb48d1e315a02a0a01857
Patrick Kelley <patrickbarrettkelley@gmail.com>
2019-05-27 14:10:41 -0700

    Adding a setup.py long description.

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 
 # from distutils.core import setup
 from setuptools import setup
+import os
+
+ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__)))
 
 tests_require = ["pytest", "coveralls"]
 dev_require = ["pre-commit", "black"]
@@ -9,7 +12,9 @@ dev_require = ["pre-commit", "black"]
 setup(
     name="policyuniverse",
     version="1.3.2.0",
-    description="AWS IAM Policy Utilities",
+    description="Parse and Process AWS IAM Policies, Statements, ARNs, and wildcards.",
+    long_description=open(os.path.join(ROOT, "README.md")).read(),
+    long_description_content_type="text/markdown",
     author="Patrick Kelley",
     author_email="patrickbarrettkelley@gmail.com",
     url="https://github.com/Netflix-Skunkworks/policyuniverse",
