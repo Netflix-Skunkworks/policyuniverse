d3dfc417a2801b291a9f40692199529999ef20de
Patrick Kelley <patrickbarrettkelley@gmail.com>
2020-06-27 17:08:21 -0700

    Updating data, version bump

--- policyuniverse/tests/test_action_categories.py
+++ policyuniverse/tests/test_action_categories.py
@@ -46,16 +46,19 @@ class ActionGroupTestCase(unittest.TestCase):
         permission_actions = actions_for_category("Permissions")
 
         for action in permission_actions:
-            if action in {"xray:getencryptionconfig"}:  # miscategorized AWS actions
+            if action in {
+                "iotsitewise:listaccesspolicies",
+                "xray:getencryptionconfig",
+                "imagebuilder:getcomponentpolicy",
+                "imagebuilder:getimagepolicy",
+                "imagebuilder:getimagerecipepolicy",
+            }:  # miscategorized AWS actions
                 continue
+
             self.assertFalse(":list" in action)
             self.assertFalse(":get" in action)
 
         for action in list_only_actions:
-            if action in {
-                "lightsail:createcloudformationstack"
-            }:  # miscategorized AWS actions
-                continue
             self.assertFalse(":put" in action)
             self.assertFalse(":create" in action)
             self.assertFalse(":attach" in action)
@@ -63,9 +66,8 @@ class ActionGroupTestCase(unittest.TestCase):
         for action in read_only_actions:
             # read actions shouldn't start with "Put" unless they are miscategorized.
             if action in {
+                "codeguru-reviewer:createconnectiontoken",
                 "ssm:putconfigurepackageresult",
-                "mobiletargeting:puteventstream",
-                "mobiletargeting:putevents",
             }:  # miscategorized AWS actions
                 continue
             # self.assertFalse(':list' in action)  # Tons of list* permissions are mis-categorized(?) as Read.
@@ -76,13 +78,17 @@ class ActionGroupTestCase(unittest.TestCase):
         for action in write_actions:
             # write actions shouldn't start with "get" unless they are miscategorized.
             if action in {
+                "appstream:getparametersforthemeassetupload",
+                "cognito-identity:getid",
+                "connect:getfederationtokens",
+                "dataexchange:getjob",
+                "glue:getmapping",
+                "lakeformation:getdataaccess",
                 "lightsail:getinstanceaccessdetails",
+                "lightsail:getrelationaldatabasemasteruserpassword",
+                "personalize:getpersonalizedranking",
                 "redshift:getclustercredentials",
                 "states:getactivitytask",
-                "cognito-identity:getid",
-                "glue:getmapping",
-                "lightsail:getrelationaldatabasemasteruserpassword",
-                "connect:getfederationtokens",
             }:  # miscategorized AWS actions
                 continue
             self.assertFalse(":get" in action)
