8cc23e408f1395e361167ca5261ca53a1695314a
scriptsrc <scriptsrc@users.noreply.github.com>
2023-06-08 10:23:45 +0000

    Updating PolicyUniverse SDFs

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ dev_require = ["pre-commit", "black"]
 
 setup(
     name="policyuniverse",
-    version="1.5.1.20230603",
+    version="1.5.1.20230608",
     description="Parse and Process AWS IAM Policies, Statements, ARNs, and wildcards.",
     long_description=open(os.path.join(ROOT, "README.md")).read(),
     long_description_content_type="text/markdown",
